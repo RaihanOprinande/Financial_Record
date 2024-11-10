9191923847f3b14d19965ad7e1f76bce6f5c092f
RaihanOprinande <opri.fahdi@gmail.com>
2024-11-10 22:13:38 +0700

    Working register

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,8 +10,10 @@ Route::get('/', function () {
 });
 
 Route::get('/login',[LoginController::class,'index']) -> name('login');
+Route::get('/register',[RegisterController::class,'index']);
 Route::get('/dashboard',[DashboardController::class,'index'])-> middleware('auth');
 
 Route::post('/login',[LoginController::class,'authenticate']);
+Route::post('/register',[RegisterController::class,'store']);
 Route::post('/logout',[LoginController::class,'logout']);
 
